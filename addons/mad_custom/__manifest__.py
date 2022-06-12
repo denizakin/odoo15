a1c9125cd107eea5c2323fd3b16702dcad42f21a
denizakin <42113126+denizakin@users.noreply.github.com>
2022-06-12 22:16:03 +0300

    mad view geliştirmeleri

---
+++ addons/mad_custom/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': '_mad_custom',
+    'version': '1.0',
+    'description': "MAD Odoo için menü ve view özelleştirmeleri",
+    'author': "Deniz Akın",
+    'installable': True,
+    'license': 'LGPL-3',
+    'version': '15.0.1',
+    'depends': ['base'],
+
+    'data': [
+        'views/mad_product_form_view.xml',
+        'views/mad_product_menu.xml'
+    ],
+}
